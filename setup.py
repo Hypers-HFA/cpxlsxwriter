134e951450f197b8b7c6e30901301d872689b484
drinks <drinksober@foxmail.com>
2017-08-17 16:06:21 +0800

    add: add compile libxlsxwriter

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from Cython.Build import cythonize
+import subprocess
+import sys
 
 from distutils.core import setup
 from distutils.extension import Extension
 
+if 'build_ext' in sys.argv:
+    subprocess.call(['wget', 'https://github.com/jmcnamara/libxlsxwriter/archive/master.zip'])
+    subprocess.call(['unzip', 'master.zip'])
+    subprocess.call(['cd', 'libxlsxwriter-master', '&&', 'make', '&&', 'make install'])
+    subprocess.call(['rm', '-rf', 'libxlsxwriter-master', '&&', 'rm', 'master.zip'])
+    subprocess.call(['cp', '/usr/local/lib/libxlsxwriter.so', '/usr/lib/'])
 ext_modules = cythonize([
     Extension("cpexcel", ["src/cpexcel.pyx"], libraries=["xlsxwriter"])])
 
