70c7c4792db3c34415eccd3fc0c926ab6165ed1d
drinks <drinksober@foxmail.com>
2017-08-18 16:34:54 +0800

    fix: fix typo

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ import sys
 from distutils.core import setup
 from distutils.extension import Extension
 
-if 'install' in sys.argv:
-    subprocess.call('wget https://github.com/jmcnamara/libxlsxwriter/archive/master.zip && unzip master.zip', shell=True)
-    subprocess.call('cd libxlsxwriter-master && make && make install', shell=True)
-    subprocess.call('rm -rf libxlsxwriter-master && rm master.zip', shell=True)
-    subprocess.call('cp /usr/local/lib/libxlsxwriter.so /usr/lib', shell=True)
-    subprocess.call('python setup.py build_ext --inplace', shell=True)
+subprocess.call('wget https://github.com/jmcnamara/libxlsxwriter/archive/master.zip && unzip master.zip', shell=True)
+subprocess.call('cd libxlsxwriter-master && make && make install', shell=True)
+subprocess.call('rm -rf libxlsxwriter-master && rm master.zip', shell=True)
+subprocess.call('cp /usr/local/lib/libxlsxwriter.so /usr/lib', shell=True)
+subprocess.call('python setup.py build_ext --inplace', shell=True)
 ext_modules = cythonize([
     Extension("cpexcel", ["src/cpexcel.pyx"], libraries=["xlsxwriter"])])
 
